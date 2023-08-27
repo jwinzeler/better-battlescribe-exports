48f5cff3937b33b63015706ab7a7e95b91c8ab69
Julian Winzeler <julian.winzeler@bwt.ee>
2023-08-27 22:43:28 +0300

    add explanation, clean up upload page style, add open button

--- src/main.js
+++ src/main.js
@@ -1,20 +1,29 @@
 class Main {
   static run(file) {
     const roster = Parser.read(file);
-    console.log(roster);
-
     const sanitizedRoster = Sanitizer.sanitize(roster);
-    console.log(sanitizedRoster);
-
     const armyData = HardcodeArmyRules.get(sanitizedRoster.faction.name);
-
     const output = Builder.getOutput(sanitizedRoster, armyData);
 
-    const download = document.getElementById('downloadButton');
-    const blob = window.URL.createObjectURL(new Blob([output]), { type: "text/html" });
-    download.setAttribute('href', blob);
-    download.setAttribute('download', `${roster.name} actually usable.html`);
+    this.setupPreview(output);
 
+    const blob = window.URL.createObjectURL(new Blob([output], { type: 'text/html' }), { type: "text/html" });
+    this.setupOpenButton(blob);
+    this.setupDownloadButton(blob, roster.name);
+  }
+
+  static setupDownloadButton(blob, name) {
+    const download = document.getElementById('downloadButton');
+    download.setAttribute('href', blob);
+    download.setAttribute('download', `Suicide-Overwatch ${name}.html`);
+  }
+
+  static setupOpenButton(blob) {
+    const open = document.getElementById('openButton');
+    open.setAttribute('href', blob);
+  }
+
+  static setupPreview(output) {
     const preview = document.getElementById('preview').contentWindow.document;
     preview.open();
     preview.write(output);
